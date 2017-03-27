dbdd166af8bc49332f0dd1e2b257e40bac59696e
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2017-03-27 12:05:06 -0300

    - Commit do Exercício 2 (enunciado na pasta 'exercise') do Capítulo 3

---
+++ EX-CAP-03-02/src/EX0302.java
@@ -0,0 +1,14 @@
+
+public class EX0302 {
+
+	public static void main(String[] args) {
+		Xpto x1 = new Xpto();
+		System.out.println("contInst = " + Xpto.getQtdInst());
+		
+		Xpto x2 = new Xpto();
+		System.out.println("contInst = " + Xpto.getQtdInst());
+		
+		Xpto x3 = new Xpto();
+		System.out.println("contInst = " + Xpto.getQtdInst());
+	}
+}
---
+++ EX-CAP-03-02/src/Xpto.java
@@ -0,0 +1,12 @@
+
+public class Xpto {
+	private static int contInst;
+	
+	public Xpto(){
+		contInst++;
+	}
+	
+	public static int getQtdInst(){
+		return contInst;
+	}
+}
