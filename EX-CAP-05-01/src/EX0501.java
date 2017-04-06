3521d2870a191cb4faec4b47c8c86ce905818699
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2017-04-06 14:37:48 -0300

    - Commit do Exercício 1 (enunciado na pasta 'exercise') do Capítulo 5

---
+++ EX-CAP-05-01/src/EX0501.java
@@ -0,0 +1,14 @@
+
+public class EX0501 {
+	public static void main(String[] args)  {   
+		Contribuinte []lst;
+		lst = Contribuinte.listaContr();
+		
+		System.out.printf("NOME                       IMPOSTO\n");
+		System.out.printf("====================       =======\n\n");
+		
+		for (Contribuinte c : lst) {
+			System.out.printf("%-20s     %9.2f\n", c.getNome(), c.calcImposto());
+		}
+	}
+}
