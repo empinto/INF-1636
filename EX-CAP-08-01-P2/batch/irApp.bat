30507966cca7600ea018fde4672e00fd8da4fffb
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2017-05-10 17:09:17 -0300

    - Commit do Exercício 1 (enunciado na pasta 'exercise' do projeto P2) do Capítulo 8

---
+++ EX-CAP-08-01-P2/batch/irApp.bat
@@ -0,0 +1,3 @@
+SET CLASSPATH=C:\Users\empinto.TECGRAF\eclipse\java-neon\eclipse\workspace\EX-CAP-08-01-P2\jar\irApp.jar
+SET PATH=C:\Program Files (x86)\Java\jre1.8.0_31\bin
+java EX0801
---
+++ EX-CAP-08-01-P2/src/EX0801.java
@@ -0,0 +1,35 @@
+import Lista.*;
+import IR.*;
+import java.io.*;
+
+public class EX0801
+{
+	public static void main(String[] args) throws IOException
+	{
+		Pilha x=new Pilha();
+		
+		Contribuinte c;
+		
+		c=new PFisica("Joao Santos",3000.00,"11111");
+		x.push(c);
+		c=new PJuridica("Lojas AA",150000.00,"10055");
+		x.push(c);
+		c=new PFisica("Maria Soares",5000.00,"22222");
+		x.push(c);
+		c=new PJuridica("Supermercados B",2000000.00,"10066");
+		x.push(c);
+		c=new PFisica("Carla Maia",1500.00,"33333");
+		x.push(c);
+		c=new PJuridica("Posto XX",500000.00,"10077");
+		x.push(c);
+		
+		while(!x.vazio())
+		{
+			c=(Contribuinte) x.pop();
+			System.out.printf("%s %9.2f\n",c.getNome(),c.calcImposto());
+		}
+		
+		System.in.read();
+	}
+	
+}
